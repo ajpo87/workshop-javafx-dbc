53364fe54228a895a58dd29af957965a9202b640
ajpo87 <antonio.oliveira87@gmail.com>
2020-01-12 14:59:20 +0000

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,16 +1,12 @@
 package gui.util;
 
-
+import java.util.Optional;
 
 import javafx.scene.control.Alert;
-
 import javafx.scene.control.Alert.AlertType;
-
-
+import javafx.scene.control.ButtonType;
 
 public class Alerts {
-
-
 
 	public static void showAlert(String title, String header, String content, AlertType type) {
 
@@ -26,4 +22,11 @@ public class Alerts {
 
 	}
 
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
+	}
 }
